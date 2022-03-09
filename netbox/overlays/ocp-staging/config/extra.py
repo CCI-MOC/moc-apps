630800e42c46aa688de85697a37d26ffd71c615b
Lars Kellogg-Stedman <lars@redhat.com>
2022-03-08 21:50:35 -0500

    Configure Netbox object storage backend on ocp-staging
    Netbox allows us to attach images to objects, which may be useful to
    help identify devices/ports/etc. By default these attachments are
    stored on the filesystem, but this complicates replicas and failover.
    
    This commit configures the S3 storage backend.  Specifically:
    
    - It allocates object storage through an `ObjectBucketClaim`
    - It uses the `Secret` and `ConfigMap` produced by the
      `ObjectBucketClaim` to configure the netbox `STORAGE_CONFIGURATION`
      option.
    - It adds an `initContainer` to the Netbox `Deployment` that takes
      care of setting an appropriate policy on the object bucket to permit
      anonymous READ access.
    
    Part of cci-moc/ops-issues#444

--- netbox/overlays/ocp-staging/config/extra.py
+++ netbox/overlays/ocp-staging/config/extra.py
@@ -1,3 +1,5 @@
+import os
+
 CORS_ORIGIN_ALLOW_ALL = True
 LOGIN_REQUIRED = True
 REMOTE_AUTH_AUTO_CREATE_GROUPS = True
@@ -10,3 +12,20 @@ REMOTE_AUTH_HEADER = "HTTP_X_FORWARDED_PREFERRED_USERNAME"
 REMOTE_AUTH_STAFF_GROUPS = ["netbox-admins"]
 REMOTE_AUTH_SUPERUSER_GROUPS = ["netbox-admins"]
 SKIP_SUPERUSER = True
+
+# See:
+# - https://netbox.readthedocs.io/en/stable/configuration/optional-settings/#storage_backend
+# - https://django-storages.readthedocs.io/en/stable/backends/amazon-S3.html
+STORAGE_BACKEND = "storages.backends.s3boto3.S3Boto3Storage"
+STORAGE_CONFIG = {
+    "AWS_STORAGE_BUCKET_NAME": os.environ["BUCKET_NAME"],
+    "AWS_S3_REGION_NAME": os.environ["BUCKET_REGION"],
+    "AWS_S3_ADDRESSING_STYLE": "path",
+    "AWS_S3_ENDPOINT_URL": "https://{}:{}".format(
+        os.environ["BUCKET_HOST"], os.environ["BUCKET_PORT"]
+    ),
+    "AWS_S3_VERIFY": "/run/secrets/kubernetes.io/serviceaccount/service-ca.crt",
+    "AWS_S3_CUSTOM_DOMAIN": "s3-openshift-storage.apps.ocp-staging.massopen.cloud/{}".format(
+        os.environ["BUCKET_NAME"]
+    ),
+}
